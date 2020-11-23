3b855994835059575aece0c00fa621b5a7f967e8
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-11-23 10:23:12 +0100

    Fixed lack of self in sensor.py

--- Python/SensorScript/Scripts/sensor.py
+++ Python/SensorScript/Scripts/sensor.py
@@ -3,10 +3,10 @@ import busio as io
 import adafruit_mlx90614
 
 class Sensor():
-    def __init__():
+    def __init__(self):
         self.i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
-        self.mlx = adafruit_mlx90614.MLX90614(i2c)
+        self.mlx = adafruit_mlx90614.MLX90614(self.i2c)
 
-    def get_temp():
-        return mlx.object_temperature()
+    def get_temp(self):
+        return self.mlx.object_temperature()
 
