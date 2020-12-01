6484daa61c0a60bd9935f318c3439533682ebdd1
Danish-Spots <danishspots@gmail.com>
2020-12-01 17:21:11 +0100

    Finished averaging  and cooldown implementation for Sensor, still needs to be moved over to SensorWSenseHat, moved alot of the values to settings.txt to avoid hardcoded values.

--- Python/SensorScript/Scripts/ReadSettings.py
+++ Python/SensorScript/Scripts/ReadSettings.py
@@ -1,13 +1,27 @@
 
 def ReadSettingsFile():
     try:
+        return_list = []
         f = open("Settings/settings.txt", "r")
-        rpi_id_text = f.readline()
+        entire_text = f.read()
+        split_text = entire_text.split("\n")
+        #split_text = [i for i in split_text if i]
+        split_text = list(filter(None, split_text))
         f.close()
-        rpi_id = rpi_id_text.split("=")[1].strip()
-        return rpi_id
+        for s in split_text:
+            if(s[0] != '#'):
+                return_list.append(ReturnValue(s))
+        return return_list
     except:
         print("No Settings File Found!")
         print("-----------------------")
         print("(To be developed)")
         return -1
+
+def ReturnValue(file_readline):
+    text = file_readline.split("=")[1].strip()
+    if text[len(text)-1] == 'f':
+        text = text[0:len(text)-1]
+        return float(text)
+    return int(text)
+
--- Python/SensorScript/Scripts/broadcast.py
+++ Python/SensorScript/Scripts/broadcast.py
@@ -6,8 +6,16 @@ import json
 
 
 class BroadCaster():
-    def __init__(self, rpi_id):
-        self.raspberryPiID = rpi_id
+    def __init__(self, settings_list):
+        self.raspberryPiID = settings_list[0]
+        self.cooldown_timer = settings_list[1]
+        self.measurement_timer = settings_list[2]
+        self.sensor_polling_rate = settings_list[3]
+        self.lower_bound_temp = settings_list[4]
+        self.upper_bound_temp = settings_list[5]
+        self.fever_temp = settings_list[6]
+        self.port_number = settings_list[7]
+        self.measurement_round = settings_list[8]
 
     async def start_broadcast(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -16,15 +24,27 @@ class BroadCaster():
         sens = Sensor()
         
         while True:
+            temps = []
             temp = sens.get_temp()
-            if (temp > 34 and temp < 43):
+            if (temp > self.lower_bound_temp and temp < self.upper_bound_temp):
+                measurement_time_end = time.time() + self.measurement_timer
+                while True:
+                    if time.time() > measurement_time_end:
+                        break
+                    temps.append(sens.get_temp())
+                    time.sleep(self.sensor_polling_rate)
+                measured_temp = sum(temps)/len(temps)
                 has_fever = False
-                if temp > 37.5:
+                if measured_temp > self.fever_temp:
                     has_fever = True
-                test_object = Test(self.raspberryPiID, temp, has_fever)
+                
+                measured_temp_f = (measured_temp * 9/5) + 32
+                print(round(measured_temp,self.measurement_round))
+                print(round(measured_temp_f,self.measurement_round))
+                test_object = Test(self.raspberryPiID, round(measured_temp,self.measurement_round), round(measured_temp_f,self.measurement_round),  has_fever)
                 json_str = json.dumps(test_object.__dict__)
                 message = bytes(str(json_str).encode())
-                server.sendto(message, ("<broadcast>", 42069))
-                time.sleep(.1)
-
+                server.sendto(message, ("<broadcast>", self.port_number))
+                time.sleep(self.cooldown_timer)
+            
 
