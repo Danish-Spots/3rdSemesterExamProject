f66ad54e902090831c8f562f0e3e0df6e1588b34
Danish-Spots <danishspots@gmail.com>
2020-12-02 11:05:15 +0100

    intermediate changes for rpi script with sensehat, needs testing by someone with a sensehat. Then I can work on the next part of it

--- Python/SensorWithSenseHatScript/Scripts/broadcast.py
+++ Python/SensorWithSenseHatScript/Scripts/broadcast.py
@@ -6,11 +6,18 @@ from .Animator import Animator
 import json
 
 
-
 class BroadCaster():
-    def __init__(self, rpi_id):
-        self.raspberryPiID = rpi_id
-        self.animator = Animator((0, 0, 0))
+    def __init__(self, settings_list):
+        self.raspberryPiID = settings_list[0]
+        self.cooldown_timer = settings_list[1]
+        self.measurement_timer = settings_list[2]
+        self.sensor_polling_rate = settings_list[3]
+        self.lower_bound_temp = settings_list[4]
+        self.upper_bound_temp = settings_list[5]
+        self.fever_temp = settings_list[6]
+        self.port_number = settings_list[7]
+        self.measurement_round = settings_list[8]
+        self.animator = Animator((0,0,0))
 
     async def start_broadcast(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -18,21 +25,33 @@ class BroadCaster():
         server.settimeout(0.2)
         #sens = Sensor()
         #---------REENABLE FOR WHEN SENSOR IS AVAILABLE
-        
         while True:
-            temp = 40#sens.get_temp()
-            #---------REENABLE FOR WHEN SENSOR IS AVAILABLE
-            if (temp > 34 and temp < 43):
+            temps = []
+            temp = 40
+            #temp = sens.get_temp()
+            #-----------REENABLE FOR WHEN SENSOR IS AVAILABLE
+            if (temp > self.lower_bound_temp and temp < self.upper_bound_temp):
+                measurement_time_end = time.time() + self.measurement_timer
+                while True:
+                    if time.time() > measurement_time_end:
+                        break
+                    #temps.append(sens.get_temp())
+                    #-----------REENABLE FOR WHEN SENSOR IS AVAILABLE
+                    time.sleep(self.sensor_polling_rate)
+                measured_temp = sum(temps)/len(temps)
                 has_fever = False
-                if temp > 37.5:
+                if measured_temp > self.fever_temp:
                     has_fever = True
                     self.animator.displayImage(self.animator.crossImage)
                 else:
                     self.animator.displayImage(self.animator.checkmarkImage)
-                test_object = Test(self.raspberryPiID, temp, has_fever)
+                
+                measured_temp_f = (measured_temp * 9/5) + 32
+                #print(round(measured_temp,self.measurement_round))
+                #print(round(measured_temp_f,self.measurement_round))
+                test_object = Test(self.raspberryPiID, round(measured_temp,self.measurement_round), round(measured_temp_f,self.measurement_round),  has_fever)
                 json_str = json.dumps(test_object.__dict__)
                 message = bytes(str(json_str).encode())
-                server.sendto(message, ("<broadcast>", 42069))
-                time.sleep(.5)
-
+                server.sendto(message, ("<broadcast>", self.port_number))
+                time.sleep(self.cooldown_timer)
 
