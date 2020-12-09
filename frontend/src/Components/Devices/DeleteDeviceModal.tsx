8000bc0b27e3aa51aa21bcebb627058290a53f82
jdhasko <dominik30000@gmail.com>
2020-12-09 15:49:24 +0100

    Delete device page desing created

--- frontend/src/Components/Devices/DeleteDeviceModal.tsx
+++ frontend/src/Components/Devices/DeleteDeviceModal.tsx
@@ -2,6 +2,8 @@ import Axios, { AxiosError, AxiosResponse } from 'axios';
 import React, { MouseEventHandler, ReactPortal, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom';
 import RaspberryPi from '../../classes/RaspberryPi'
+import "../../css/deleteDevice.scss";
+
 
 interface DeleteDeviceProps {
     closeModal: () => void
@@ -26,12 +28,15 @@ export const DeleteDeviceModal: React.FC<DeleteDeviceProps> = ({showModal, close
     
     if (!showModal) return null
     return ReactDOM.createPortal(
-        <div className="OverlayContainer">
-            <div className="ModalContainer">
+        <div className="OverlayContainer-delete">
+            <div className="ModalContainer-delete">
                 <h3>Are you sure that you want to delete this device from the system?</h3>
-                <button onClick={closeModal}>X</button>
+                
+                <div className="deleteButtonsContainer">
                 <button onClick={closeModal}>No</button>
                 <button onClick={DeleteDevice}>Yes</button>
+                </div>
+            
             </div>
         </div>, document.getElementById("portal") as HTMLElement)
     
